28fe15e14f5d8955ceb750cc09646fde084c614f
Somak Somanchi <somaksomanchi1krishna@gmail.com>
2024-05-30 06:39:57 +0000

    red rectangle game

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
-print("hello")
+import pygame
+
+pygame.init()
+
+#window dimensions
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+player = pygame.Rect((300, 250), (50, 50))
+#game loop
+run = True
+while run == True:
+  #event handler
+
+  screen.fill((0, 0, 0))
+  pygame.draw.rect(screen, (255, 0, 0), player)
+
+  key = pygame.key.get_pressed()
+  if key[pygame.K_a] == True:
+    player.move_ip(-1, 0)
+  elif key[pygame.K_d] == True:
+    player.move_ip(1, 0)
+  elif key[pygame.K_w] == True:
+    player.move_ip(0, -1)
+  elif key[pygame.K_s] == True:
+    player.move_ip(0, 1)
+  
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      run = False
+
+  pygame.display.update()
+pygame.quit()
